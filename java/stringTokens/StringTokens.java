5f42a5ad97e30cb8bc0e04b3ff029d6b975a8b87
Nathan Kurz <natcom@Nathans-MacBook-Air.local>
2018-05-07 00:17:32 -0400

    String Tokens

---
+++ java/stringTokens/StringTokens.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+/**
+ * Dev: Nathan Kurz
+ * 
+ * Program takes a String as input, counts the number of Tokens, and prints each token
+ * individually on its own line.
+ */
+public class StringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+
+        // Trim and split the string using the delimeters
+        String[] sArray = s.trim().split("[ !,?\\._'@]+", 0);
+        
+        if (sArray[0] == "") {
+            System.out.println(0);
+        } else {
+            System.out.println(sArray.length);
+        }
+
+        for (int i = 0; i < sArray.length; i++) {
+            System.out.println(sArray[i]);
+        }
+
+        scan.close();
+    }
+}
